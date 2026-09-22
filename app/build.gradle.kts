842ae68ddc7d863af26d3ca28bdc35cfe57f93df
Duckxyz06 <nguyenvietduc06xyz@gmail.com>
2026-09-22 11:19:12 +0700

    build: target Android 16 for Play submission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.duckxyz.zgm"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.duckxyz.zgm"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 2
         versionName = "0.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
