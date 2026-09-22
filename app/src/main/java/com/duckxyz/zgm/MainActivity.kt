e2219c22afb3a30f5648946e4dc538ba145b9654
Duckxyz06 <nguyenvietduc06xyz@gmail.com>
2026-09-22 10:01:47 +0700

    feat: add app entry activity

---
+++ app/src/main/java/com/duckxyz/zgm/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.duckxyz.zgm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.duckxyz.zgm.ui.ZgmApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { ZgmApp() }
+    }
+}
